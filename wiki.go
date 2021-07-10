13f2be0cb4c4a814ed70e1cd4b74bb435e79650b
Richard Emate <richardemate@gmail.com>
2021-07-10 05:16:26 +0100

    feat: added new routes

--- wiki.go
+++ wiki.go
@@ -6,15 +6,21 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
 type Page struct {
 	Title string
 	Body  []byte
+	Route string
+}
+
+type Archive struct {
+	Wikis []string
 }
 
 var templates = template.Must(template.ParseFiles("tmpl/view.html", "tmpl/edit.html", "tmpl/home.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$|^/(create)$|^/$")
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9-]+)$|^/(create)$|^/$")
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
@@ -28,41 +34,57 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 
-	return &Page{Title: title, Body: body}, nil
+	return &Page{Title: strings.Title(title), Body: body}, nil
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+func joinHyphenatedTitles(title string) string {
+	containsHyphen := strings.Contains(title, "-")
+	if containsHyphen {
+		splittedTitle := strings.Split(title, "-")
+		return strings.Join(splittedTitle, " ")
+	}
+	return title
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	title = joinHyphenatedTitles(title)
 	p, err := loadPage(title)
 	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		http.Redirect(w, r, "/edit/"+strings.ToLower(title), http.StatusFound)
 	}
 	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	urlTitle := title
+	title = joinHyphenatedTitles(title)
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
 
+	p.Route = urlTitle
+
 	renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	urlTitle := title
+	title = joinHyphenatedTitles(title)
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+	http.Redirect(w, r, "/view/"+strings.ToLower(urlTitle), http.StatusFound)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -73,19 +95,36 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	renderTemplate(w, "home", &Page{})
+	files, _ := ioutil.ReadDir("data")
+
+	var fileNames []string
+
+	for _, file := range files {
+		extIndex := strings.Index(file.Name(), ".txt")
+		fileName := file.Name()[:extIndex]
+		fileNames = append(fileNames, strings.Title(fileName))
+	}
+
+	renderTemplate(w, "home", &Archive{Wikis: fileNames[:6]})
 }
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
-	p := &Page{Title: title, Body: []byte("")}
+	p := &Page{Title: strings.ToLower(title), Body: []byte("")}
 	err := p.save()
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+	splittedTitle := strings.Split(title, " ")
+
+	if len(splittedTitle) > 1 {
+		// fmt.Println(strings.Join(splittedTitle, "-"))
+		http.Redirect(w, r, "/edit/"+strings.ToLower(strings.Join(splittedTitle, "-")), http.StatusFound)
+	}
+
+	http.Redirect(w, r, "/edit/"+strings.ToLower(title), http.StatusFound)
 }
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
@@ -100,13 +139,6 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 		fn(w, r, m[2])
 	}
 }
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	p1 := &Page{Title: "First Golang Page", Body: []byte("This is a sample, you can treat this however you want")}
-// 	p1.save()
-// 	p2, _ := loadPage("First Golang Page")
-// 	fmt.Fprintf(w, "Hi there, I love %s! %s", r.URL.Path[1:], string(p2.Body))
-// }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
